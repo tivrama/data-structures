f5eb55afeef9d0158c7207e6d9f605901f37e8d7
tivrama <tivrama@yahoo.com>
2016-05-04 12:31:56 -0700

    Three tests passing on functional-shared.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // make a new stack
+  var giraffe = {};
+  // extend with methods
+  _.extend(giraffe, Stack.stackMethods);
+  // console.log('giraffe: ', giraffe);
+  // console.log('methods: ', Stack.stackMethods);
+  // return the object we've built
+  return giraffe;
 };
 
-var stackMethods = {};
+Stack.stackMethods = {};
 
+// create an index to access in our methods
+Stack.stackMethods.index = 0;
+
+// 1. Check size
+Stack.stackMethods.size = function(){
+  return Stack.stackMethods.index;
+};
+
+// 2. Push
+Stack.stackMethods.push = function(value){
+  // 1. add a key-value pair to the stack, 2. increment the index
+  Stack[Stack.stackMethods.index++] = value;
+};
+
+// 3. Pop
+Stack.stackMethods.pop = function(){
+  // 1. decrement the index, 2. return the last indexed item
+  Stack.stackMethods.index = Stack.stackMethods.index - 1;
+  console.log(Stack.stackMethods.size());
+  return Stack[Stack.stackMethods.index];
+};
 
