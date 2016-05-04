c6061d20a226bc890421f04e8206ee8d7d59ec97
tivrama <tivrama@yahoo.com>
2016-05-04 11:23:13 -0700

    Class notes

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[index++] = value;
   };
 
   someInstance.pop = function(){
+    if(index){
+      return storage[--index];
+    }
   };
 
   someInstance.size = function(){
+    return index;
   };
 
   return someInstance;
