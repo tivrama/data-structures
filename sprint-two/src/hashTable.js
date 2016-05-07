1953e2651a55cdd253bca431e95a2a2d35f2281b
tivrama <tivrama@yahoo.com>
2016-05-07 16:36:34 -0700

    hashTable complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,6 @@
 var HashTable = function(){
   this._limit = 8;
+  //create an instance of the limited array constuctor using the limit set above as the parameter (this will be an object), and store it at _storage
   this._storage = LimitedArray(this._limit);
 };
 
@@ -8,16 +9,21 @@ HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   // console.log('index from insert: ', i);
 
-  // the array at that index
-  var arr = this._storage.get(i);
+  // the object at that index
+  var obj = this._storage.get(i);
 
-  // check if there is an array at that index already
-  if (Array.isArray(arr)) {
-    arr.push(v);
-  // if it's not an array yet, add the value as the first item in an array at that index
+  // check if there is an object at that index already
+  if (typeof obj === 'object') {
+    //assign the object the key value pair
+    obj[k] = v;
+  // if it's not an object yet
   } else {
-    // access the set method in the limited array instance, and pass in our index and value inside a new array
-    this._storage.set(i, [v]);
+    // create an empty object
+    var objAtIndex = {};
+    //and assign it the target key value pair
+    objAtIndex[k] = v;
+    // add this object at the index
+    this._storage.set(i, objAtIndex);
   }
 
 };
@@ -25,22 +31,27 @@ HashTable.prototype.insert = function(k, v){
 HashTable.prototype.retrieve = function(k){
   // index we get from the hash function, converting our key 'k' to a number within our limit
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // console.log('index from retrieve: ', i);
   // access the get method in the limited array instance, and pass in our index
-  var arr = this._storage.get(i);
-  for (var j=0; j < arr.length; j++) {
-    if (arr[j] === k) {
-      return arr[j];
-    }
+  var obj = this._storage.get(i);
+  //if no object is found at this index
+  if (obj === undefined) {
+    return null;
+  //if there is an object found, return the value at the key
+  } else {
+    return obj[k];
   }
 };
 
 HashTable.prototype.remove = function(k){
   // index we get from the hash function, converting our key 'k' to a number within our limit
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // access the set method in the limited array instance, passing in our index, and reset the value to undefined
-  this._storage.set(i, null);
+  //get the object at this index and store to a var
+  var obj = this._storage.get(i)
+  //set the key in the object at found index to null
+  obj[k] = null;
+
 };
+
 
 
 
