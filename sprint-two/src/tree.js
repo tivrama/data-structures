64097858d702dddad5820b0f55d9b356dd7a9b6a
tivrama <tivrama@yahoo.com>
2016-05-06 17:30:59 -0700

    Tree complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,24 +3,52 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me -- now fixed
+  //add the methodes to each instance of Tree
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
   // your code here
-  // newTree.children = null;  // fix me
+  // newTree.children = [];  // changed null to []
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  //create a new instance of Tree using the value passed in and save to a var
+  var newChild = Tree(value);
+
+  //access the children property of the object add child is acting on (this) and push this new tree on to the end
+  this.children.push(newChild);
+
+
 
 };
 
 treeMethods.contains = function(target){
-
+  //create a state var and defualt to false
+  var state = false;
+  console.log(this);
+  //create a subroutine to recursivly check the children
+  var targetSearch = function(currentTree) {
+    //base case: chage state var to true if target = value
+    if(currentTree.value === target) {
+      state = true;
+    }
+    //recursive case: check each item in the children array
+    else {
+      console.log(currentTree.children);
+      for(var i = 0; i < currentTree.children.length; i++) {
+        targetSearch(currentTree.children[i]);
+      }
+    }
+  }
+  targetSearch(this);
+  //return state var
+  return state;
 };
 
 
